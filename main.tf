a23e7ec0c9cdc43c8feb44af805db3bd4080a2da
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2024-10-28 15:59:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+        region  = "us-east-1"
+}
+
+resource "aws_instance" "Sintance" {
+        ami  =  "ami-0866a3c8686eaeeba"
+        instance_type     = "t2.micro"
+        key_name = "Demo"
+        associate_public_ip_address = true
+tags = {
+        Name = "Myinstance"
+}
+}
